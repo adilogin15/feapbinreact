bd56d0f8dda84ffa0e3088d95f7937ece01b73ef
Aditya Sharma <aditya@webmobi.in>
2020-05-28 23:54:12 +1000

    Initial Commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from '../bar/MainPage';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <MainPage />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
